26594144cd8156c53352e74d51be21421c58f662
Brandon Tylke <brandon@tylke.org>
2018-12-10 22:03:09 -0500

    Altering the StressTest.py script to generate more interesting trees.

--- StressTest.py
+++ StressTest.py
@@ -2,19 +2,30 @@
 
 import requests
 import time
+import random
 from multiprocessing.pool import Pool
 
-pool = Pool(10) 
+pool = Pool(10)
 
 def createDependency(id):
 	print ("Creating child: ", id)
 	requests.post("http://127.0.0.1:8080/?name=child&version=%d&parentName=parent&parentVersion=0" % id)
 
+def createRandomParentDependency(id):
+	parent_id = random.randint(1, 100)
+	print ("Creating child: ", id, "of parent", parent_id)
+	requests.post("http://127.0.0.1:8080/%d?name=child&version=%d" % (parent_id,id))
+
 def main():
 	# create a top level parent
 	requests.post('http://127.0.0.1:8080/?name=parent&version=0')
 
-	pool.map(createDependency, range(0, 1000000))
+	# create the first 100 children
+	for i in range(0, 99):
+		createDependency(i)
+
+	# create a few more with random parents
+	pool.map(createRandomParentDependency, range(100, 1000000))
 	pool.close() 
 	pool.join() 
 		
